3b9a964e7b7607904cc7bd22fb012099f358ed79
Faridekhalili <khalili.f@northeastern.edu>
2024-06-06 14:02:21 -0400

    Fix and count

--- day19/src/App.js
+++ day19/src/App.js
@@ -4,19 +4,20 @@ import './App.css';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 const App = memo(function App() {
+  console.log(window.globalCount++);
   const [data, setData] = useState({});
   const city = useRef('Paris');
   useEffect(() => {
     handleClick();
   }, []);
   const handleClick = () => {
-    axios(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=8b056cc4938d7e9a8c51a5162df93618`).then(res => setData(res.data)).catch(err => console.log(err));
-    document.body.style.backgroundImage = "url('https://source.unsplash.com/1920x1080/?" + city.value + "')";
+    axios(`https://api.openweathermap.org/data/2.5/weather?q=${city.current.value}&appid=8b056cc4938d7e9a8c51a5162df93618`).then(res => setData(res.data)).catch(err => console.log(err));
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1920x1080/?" + city.current.value + "')";
   };
   console.log(data);
   return <div className='container'>
       <div className='input-box'>
-        <input placeholder='Enter a City...' value={city.value} ref={dummy}></input>
+        <input placeholder='Enter a City...' ref={city}></input>
         <button onClick={handleClick}>X</button>
       </div>
       <p className='temp'>{data.main ? Math.round(data.main.temp - 273.15) + '°C' : ''}</p>
